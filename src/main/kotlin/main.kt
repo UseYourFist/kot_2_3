0ada4d722c5028c1c79981d23f19f3ee210ccd67
UseYourFist <spr47@mail.ru>
2022-04-24 13:36:50 +0700

    made a calculation for a regular buyer separately

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,24 +3,16 @@ fun main() {
     val newPurchase = 1000
     val regularBuyer = true
     val sum = if (oldPurchase in 1..1000) {
-        if (regularBuyer) {
-            newPurchase * 0.99
-        } else {
             newPurchase
-        }
     } else if (oldPurchase in 1001..10_000) {
-        if (regularBuyer) {
-            (newPurchase - 100) * 0.99
-        } else {
             newPurchase - 100
-        }
     } else {
-        if (regularBuyer) {
-            (newPurchase * 0.95) * 0.99
-        } else {
             newPurchase * 0.95
-        }
     }
-    println(sum)
+    if (regularBuyer) {
+        println((sum.toInt() * 0.99).toInt())
+    } else {
+        println(sum)
+    }
 }
 
